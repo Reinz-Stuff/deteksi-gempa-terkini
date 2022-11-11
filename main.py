a63dbb5474531124a90e66f4687167e48bb0aa42
Fauzi kurniawan <ozilpym123@gmail.com>
2022-11-11 14:25:37 +0700

    new variable added

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ Aplikasi Deteksi Gempa terkini
 import gempa_terkini
 
 if __name__ == "__main__":
-    print('Aplikasi utama')
+    print(f'Aplikasi utama menggunakan package yang memiliki deskripsi {gempa_terkini.description}')
     result = gempa_terkini.ektraksi_data()
     gempa_terkini.tampilkan_data(result)
